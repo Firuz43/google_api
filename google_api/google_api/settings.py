ec6bc1af20233aa2a336255f786f6d4d383e714d
Firuz43 <firuz1199@mail.ru>
2022-03-13 15:43:38 +0400

    Settings changes and main app

--- google_api/google_api/settings.py
+++ google_api/google_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -117,7 +119,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+
+GOOGLE_API_KEY = "AIzaSyDrecpaqQGNXdeNUVfFFVubcN9uRwlufC8"
+
+RECAPTCHA_KEY = "6LeWCdgeAAAAACNlTNsqoOCX8Sxjg_aHpAh8yV3r"
+
+RECAPTCHA_SECRET_KEY = "6LeWCdgeAAAAAP0R4vvVpz9JoBY8jI_iaie2VdAf"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
